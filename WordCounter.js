22e653b993b9e7d75d24d8334bbbe7bd08202d8f
Muhammad Atiq Ur Rehman <146697001+Atiq-Developer@users.noreply.github.com>
2024-06-05 22:51:29 +0500

    Initial commit

---
+++ WordCounter.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node 
+//This line is called shebang,ehich tells the operating system to run it with node.js file.
+// Import the inquirer module ,which is command line interface for Node.js.
+import inquirer from "inquirer";
+// Declare a constant answer and assign it to the the result of inquirer.prompt function
+const answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter Your Phrase/sentence to count the word : "
+    }
+]);
+const words = answer.sentence.trim().split(" ");
+// trim is a method of string that can Removes whitespace from both ends of a string.
+// Divides a string into an array of substrings using a specified separator.
+/*Now print the array of words to the console*/
+console.log(words);
+//Print the word count of given sentence to console
+console.log(`Your total word of the given sentence is equlas to : ${words.length}`);
